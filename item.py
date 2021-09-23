622b089b8d64408b2f72e49c5f4e17758e385a4d
yabouseada <y.abouseada@gmail.com>
2021-09-23 16:25:10 +0200

    pending review

--- item.py
+++ item.py
@@ -1,3 +1,4 @@
+from sqlite3.dbapi2 import Cursor
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 import sqlite3
@@ -12,36 +13,60 @@ class Item(Resource):
             required=True,
             help="This field cannot be left blank!"
         )
+
+
     @jwt_required()
     def get(self,name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
 
-        query = "SELECT * FROM items HHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
+            item = self.find_by_name(name)
+            if item:
+                return item
         
-        connection.close()
+            return {'message':'Item not found'},404
 
-        if row:
-            return{'item':{'name':row[0],'price':row[1]   }     } 
+
     
-        return {'message':'Item not found'},404
+    @classmethod
+    def find_by_name(cls,name):
+
+            connection = sqlite3.connect('data.db')
+            cursor = connection.cursor()
+
+            query = "SELECT * FROM items WHERE name=?"
+            result = cursor.execute(query, (name,))
+            row = result.fetchone()
+            connection.close()
+
+            if row:
+                return{'item':{'name':row[0],'price':row[1]}} 
+    
+    
     
     def post(self,name):
-        if next(filter(lambda x: x['name']==name,items),None):
-            return{'message':"An item with name'{}'already.".format(name)}, 400
+            if self.find_by_name:
+                return{'message':"An item with name'{}'already exists.".format(name)}, 400
 
-        data = Item.parser.parse_args()
-        item ={'name':name,'price': data['price']}
-        items.append(item)
-        return item, 201
+            data = Item.parser.parse_args()
+
+            item ={'name':name,'price': data['price']}
+
+            connection = sqlite3.connect('data.db')
+            cursor = connection.cursor()
+
+            query = "INSERT INTO items VALUES (?,?)"
+            cursor.execute(query, (item['name'],item['price']))
+
+            connection.commit()
+            connection.close()
+
+
+            return item, 201
 
     def delete(self,name):
-        global items
-        items = list(filter(lambda x: x['name']!=name, items))
-        return{'message':'Item delete'}
-    
+            global items
+            items = list(filter(lambda x: x['name']!=name, items))
+            return{'message':'Item delete'}
+        
     
     def put(self,name):
         data = Item.parser.parse_args()
